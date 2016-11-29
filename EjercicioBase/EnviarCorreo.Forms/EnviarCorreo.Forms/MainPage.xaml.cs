ce7ca7ceab2921b406a07cf3b8fcdd812d988c1c
Amin Espinoza <doomsday5@hotmail.com>
2016-11-29 13:56:34 -0600

    Segundo commit
    Funcionalidad de Xamarin Forms integrada

--- EjercicioBase/EnviarCorreo.Forms/EnviarCorreo.Forms/MainPage.xaml.cs
+++ EjercicioBase/EnviarCorreo.Forms/EnviarCorreo.Forms/MainPage.xaml.cs
@@ -13,5 +13,10 @@ namespace EnviarCorreo.Forms
         {
             InitializeComponent();
         }
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+
+        }
     }
 }
