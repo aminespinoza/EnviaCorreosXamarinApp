edfaf9702125d273bb82c65ea64e9bde86925124
Amin Espinoza <doomsday5@hotmail.com>
2016-11-29 13:34:33 -0600

    Segundo commit
    La funcionalidad de la demo para Xamarin nativo ha sido incluida.

---
+++ EjercicioBase/EnviarCorreo.Nativo/Classes/Registros.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+namespace EnviarCorreo.Nativo.Classes
+{
+    public class Registros
+    {
+        public int id { get; set; }
+        public string correo { get; set; }
+        public string identificador { get; set; }
+    }
+}
---
+++ EjercicioBase/EnviarCorreo.Nativo/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using EnviarCorreo.Nativo.Classes;
+
+namespace EnviarCorreo.Nativo
+{
+    [Activity(Label = "EnviarCorreo.Nativo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            Button button = FindViewById<Button>(Resource.Id.btnReportar);
+
+            button.Click += btnReportar_Click;
+        }
+
+        private void btnReportar_Click(object sender, EventArgs e)
+        {
+            ServiceHelper.InsertarCorreo("micorreo@servicio.com");
+            
+        }
+    }
+}
+
